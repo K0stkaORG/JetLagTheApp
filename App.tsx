8c698277e050034f67225235e776c0678ff9e545
K0stka <91200807+K0stka@users.noreply.github.com>
2025-05-28 13:59:29 +0200

    Remove template code

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
 import './global.css';
+
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
       <StatusBar style="auto" />
     </>
   );
