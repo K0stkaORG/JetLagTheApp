954129b569d801f31b6e00d84a8878b7a6360fcb
DaalbuCZ <daalbu@post.cz>
2025-06-12 10:52:35 +0200

    implement KeyboardAvoidingView for auth flow

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,4 +1,4 @@
-import { KeyboardAvoidingView, View } from "react-native";
+import { KeyboardAvoidingView, Platform, View } from "react-native";
 import { Redirect, Slot } from "expo-router";
 
 import Logo from "~/assets/Logo";
@@ -11,7 +11,7 @@ const Layout = () => {
     if (isAuthenticated) return <Redirect href="/" />;
 
     return (
-        <KeyboardAvoidingView>
+        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
             <SafeAreaView className="bg-jetlag-blue">
                 <View className="h-screen-safe flex w-screen items-center justify-start gap-5 pt-5">
                     <Logo className="h-72 w-72" />
