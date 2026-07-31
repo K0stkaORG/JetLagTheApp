fb243ebdbce544696ae365dbf5ff90b5a1ba2faf
K0stka <www.honza.90@gmail.com>
2026-08-01 01:39:53 +0200

    Fix

--- apps/backend/drizzle/0014_bored_energizer.sql
+++ apps/backend/drizzle/0014_bored_energizer.sql
@@ -1,4 +1,4 @@
 ALTER TABLE "datasets" RENAME COLUMN "data" TO "input";--> statement-breakpoint
-DELETE FROM "datasets" WHERE 1;
-DELETE FROM "datasets_metadata" WHERE 1;
+DELETE FROM "datasets" WHERE true;
+DELETE FROM "datasets_metadata" WHERE true;
 ALTER TABLE "datasets" ADD COLUMN "parsed" jsonb NOT NULL;
